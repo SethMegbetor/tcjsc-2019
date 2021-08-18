6c9137d1c772c787631cd9341861278e1fadf1ac
SethMegbetor <sethmegbetor@gmail.com>
2021-08-18 12:05:20 +0000

    for wednesday

---
+++ basics/script.js
@@ -0,0 +1,72 @@
+// console.log("hello world");
+
+// var firstName = "John";
+
+// var lastName = "Smith";
+
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher'
+
+// console.log(job);
+
+// var firstName = "John";
+// var age = 28;
+
+// //Type coercion
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(
+//   firstName +
+//     " is a " +
+//     age +
+//     " years old " +
+//     job +
+//     "  is he married? " +
+//     isMarried
+// );
+
+// //variable mutation
+// age = "twenty eight";
+// job = "driver";
+
+// alert(
+//   firstName +
+//     " is a " +
+//     age +
+//     " years old " +
+//     job +
+//     "  is he married? " +
+//     isMarried
+// );
+
+// var lastName = prompt("what is his last name?");
+
+/*******************
+ *Basic operators
+ */
+
+var now, yearJohn, yearMark;
+now = 2021;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
